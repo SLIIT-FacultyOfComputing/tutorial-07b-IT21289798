daefe8c6e234d7de9b37d67175410ec899e060a6
Malith Geevinda <99596289+IT21289798@users.noreply.github.com>
2022-05-17 18:55:07 +0000

    I did it.

--- Exercise01.cpp
+++ Exercise01.cpp
@@ -2,17 +2,22 @@
 #include "Item.h"
 using namespace std;
 
+
+
 // DO NOT CHANGE THIS CODE
 
+
+
 int main() {
-    Item myItem;
-    myItem.display();
-    Item myItem2(100, 1000);
-    myItem2.setDiscount(20);
-    myItem2.display();
-    
-    return 0;
+Item myItem;
+myItem.display();
+Item myItem2(100, 1000);
+myItem2.setDiscount(20);
+myItem2.display();
+
+return 0;
 }
 
-// DO NOT CHANGE ABOVE CODE
 
+
+// DO NOT CHANGE ABOVE CODE
